b2ae42265b3451bab9067c467d633d21be06722e
heidga <llheidgall@github.com>
2025-12-19 18:16:52 +0800

    Radio init

--- build.sh
+++ build.sh
@@ -1,5 +1,18 @@
 #!/bin/zsh
+<<<<<<< HEAD
 
+=======
+# build the packages
+check_dir=..
+entries=("$check_dir"/*/)
+if [[ ${#entries[@]} -eq 1 ]]; then
+    cd ..
+    # git clone the packages
+    git clone https://github.com/stm32f303ret6/livox_laser_simulation_RO2.git
+    git clone https://github.com/Livox-SDK/livox_ros_driver2
+    colcon build --packages-select livox_ros_driver2
+fi
+>>>>>>> 53ee389 (Radio init)
 # build all the packages
 colcon build
 # source the setup file
---
+++ try.sh
@@ -0,0 +1,7 @@
+this_file_dir=$(dirname "$0")
+echo $this_file_dir
+check_dir=..
+echo $(dirname "$0")
+cd $check_dir
+echo $(dirname "$0")
+echo $(pwd)
